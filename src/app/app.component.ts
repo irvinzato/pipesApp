96ee12cc02d5139440fb3a6fc26ecd7536e7d6fa
irvinzato <multizato@hotmail.com>
2022-05-26 18:29:08 -0500

    Implementando estilos generles de PrimeNg en styles.css, modulos y utilizandolos en el html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'pipesApp';
   valor: number = 1000;
+  obj = {
+    nombre: 'Irving',
+    apellido: 'Rivera'
+  }
 }
