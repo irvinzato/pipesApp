edd31db9f934d3b14de2b7f4487b668badbcc724
irvinzato <multizato@hotmail.com>
2022-05-26 18:04:34 -0500

    Instalacion de Prime Ng y estilo elegido en angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  title: string = 'pipesApp';
+  valor: number = 1000;
 }
