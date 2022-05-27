0c8fcec9425bcd638b446f4d70ae1285fdfb8554
irvinzato <multizato@hotmail.com>
2022-05-26 22:16:56 -0500

    Implementando rutas en el nav de primeNg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { OrdenarComponent } from './ventas/pages/ordenar/ordenar.component';
+import { BasicosComponent } from './ventas/pages/basicos/basicos.component';
 import { NoComunesComponent } from './ventas/pages/no-comunes/no-comunes.component';
 import { NumerosComponent } from './ventas/pages/numeros/numeros.component';
-import { BasicosComponent } from './ventas/pages/basicos/basicos.component';
+import { OrdenarComponent } from './ventas/pages/ordenar/ordenar.component';
 
 const routes: Routes = [
   { path:'' , component: BasicosComponent, pathMatch:'full' },
