da5791d0b7abe7c6829908af879cb77d4f828988
irvinzato <multizato@hotmail.com>
2022-05-27 19:53:23 -0500

    Implementacion dinamica en pipes no comunes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -5,11 +5,42 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './no-comunes.component.html',
   styleUrls: ['./no-comunes.component.css']
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  // i18nSelect
+  nombre: string = 'Irving';
+  genero: string = 'masculino';
 
-  ngOnInit(): void {
+  invitacionObj = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
+  }
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Juan', 'Norma', 'Ruiz'];
+  clientesMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.' ,
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  cambiarNombre() {
+    if( this.genero == 'masculino' ) {
+      this.nombre = 'Jade';
+      this.genero = 'femenino'
+    } else {
+      this.nombre = 'Irving';
+      this.genero = 'masculino'
+    }
+ 
+  }
+
+  borrarCliente() {
+    this.clientes.pop();
+  }
+
+  agregarCliente() {
+    this.clientes.push('ClienteX');
   }
 
 }
